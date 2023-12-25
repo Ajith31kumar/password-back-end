a0697315593209b65fa72a9ed40dbea30f8284c5
ajith <142565103+Ajith31kumar@users.noreply.github.com>
2023-12-25 23:15:10 +0530

    first commit

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ const URL = process.env.DB;
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(express.json());
 
-// mongoose
-//   .connect(URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-  // .then(() => {
-  //   console.log("MongoDB Connected");
-  // })
-  // .catch((error) => {
-  //   console.error("MongoDB connection error:", error);
-  // });
+mongoose
+    .connect(URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("MongoDB Connected");
+    })
+    .catch((error) => {
+        console.error("MongoDB connection error:", error);
+    });
 
 const userRouter = require("./routes/user");
 app.use("/user", userRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server running at port ${PORT}`);
+    console.log(`Server running at port ${PORT}`);
 });
