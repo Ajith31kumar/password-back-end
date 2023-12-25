0e96c0af028db2662781b2ddbc039334dd69d504
ajith <142565103+Ajith31kumar@users.noreply.github.com>
2023-12-25 23:00:50 +0530

    first commit

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv").config({ path: ".env" });
 const cors = require("cors");
-const app = express();
-app.use(cors());
-app.use(express.json());
+require("dotenv").config({ path: ".env" });
 
+const app = express();
 const PORT = process.env.PORT || 4100;
 const URL = process.env.DB;
 
-// mongoose
-//   .connect(URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log('MongoDB Connected');
-//   })
-//   .catch((error) => {
-//     console.error('MongoDB connection error:', error);
-//   });
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(express.json());
 
-const userRouter = require('./routes/user');
-app.use('/user', userRouter);
+mongoose
+  .connect(URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
+
+const userRouter = require("./routes/user");
+app.use("/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
